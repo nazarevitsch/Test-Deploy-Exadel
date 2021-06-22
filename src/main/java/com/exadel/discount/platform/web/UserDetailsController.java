5ffa03b6068c70b88a17d34cdab3010e4bed645b
nazarevitsch <bidaritter1997@gmail.com>
2021-06-22 16:49:04 +0300

    logs for deploy 2

--- src/main/java/com/exadel/discount/platform/web/UserDetailsController.java
+++ src/main/java/com/exadel/discount/platform/web/UserDetailsController.java
@@ -16,6 +16,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/user_details")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class UserDetailsController {
 
     @Autowired
