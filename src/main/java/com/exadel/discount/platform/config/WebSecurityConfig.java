52f263e9cd6a305301766aea2f5a4e6925c2d1bb
nazarevitsch <bidaritter1997@gmail.com>
2021-06-22 16:36:52 +0300

    deploy

--- src/main/java/com/exadel/discount/platform/config/WebSecurityConfig.java
+++ src/main/java/com/exadel/discount/platform/config/WebSecurityConfig.java
@@ -42,6 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .antMatchers("/user/login", "/",
                         "/user/validate_token",
+
                         "/v2/api-docs",
                         "/configuration/ui",
                         "/swagger-resources/**",
