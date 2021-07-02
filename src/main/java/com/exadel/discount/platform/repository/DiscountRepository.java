e425d0bd820cd748f6a13f45fcad549666ac6de2
nazarevitsch <bidaritter1997@gmail.com>
2021-07-02 12:34:34 +0300

    samll fixes

--- src/main/java/com/exadel/discount/platform/repository/DiscountRepository.java
+++ src/main/java/com/exadel/discount/platform/repository/DiscountRepository.java
@@ -23,9 +23,9 @@ public interface DiscountRepository extends JpaRepository<Discount, UUID>, Pagin
             " join vendor_location on dvl_vendor_location_id = vl_id" +
             " where (:vendor_ids is null or d_vendor_id in :vendor_ids)" +
             " and (:category_id is null or d_category_id = :category_id)" +
-            " and (:subCategoriesIds is null or dsc_sub_category_id in :subCategoriesIds)" +
+            " and (:sub_categories_ids is null or dsc_sub_category_id in :sub_categories_ids)" +
             " and (:country is null or (vl_country = :country and vl_city = :city))" +
-            " and (:searchWord = null or (d_name ~* :search_word or d_full_description ~* :searchWord))",
+            " and (:search_word = null or (d_name ~* :search_word or d_full_description ~* :search_word))",
             nativeQuery = true)
     Page<Discount> findAllByFilters(@Param("vendor_ids") List<UUID> vendorIds, @Param("category_id") UUID categoryId,
                                     @Param("sub_categories_ids") List<UUID> subCategoriesIds, @Param("country") String country,
